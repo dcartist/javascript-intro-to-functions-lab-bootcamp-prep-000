ce25fd6062ffa48db79bef8fd341c3028dd4c579
dcartist <pbannerman13@gmail.com>
2018-05-17 00:44:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function shout(string) {
 }
 function whisper(string){
   //return (string).toLowerCase()
-  console.log(string.toLowerCase())
+  return(string.toLowerCase())
 }
 function logShout(string){
   console.log(string.toUpperCase())
