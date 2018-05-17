b348ce76dee97d00088e591aeb929a9778a7b8af
dcartist <pbannerman13@gmail.com>
2018-05-17 00:37:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 function shout(string) {
   return (string).toUpperCase()
 }
+function whisper(string){
+  //return (string).toLowerCase()
+  console.log(string.toLowerCase())
+}
+function logShout(string){
+  console.log(string.toUpperCase())
+  //return (string)
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase){
+    returns ("I can't hear you! if ${string} is lowercase:")
+  } else if ()
+
+}
